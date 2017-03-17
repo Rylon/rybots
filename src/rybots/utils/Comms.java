80430c0ddff47d2a1102e817e0bb770ddc9fd15f
Ryan Conway <ryan@rjc.cc>
2017-03-17 16:35:51 +0000

    Add two channels for x/y coordinates of enemy soldiers.

--- src/rybots/utils/Comms.java
+++ src/rybots/utils/Comms.java
@@ -4,5 +4,7 @@ public class Comms {
 
   public static final int SCOUT_CONSTRUCTION_ENABLED = 4;
   public static final int SCOUT_HEARTBEAT_CHANNEL    = 5;
+  public static final int SOLDIER_ENEMY_SPOTTED_X_CHANNEL = 10;
+  public static final int SOLDIER_ENEMY_SPOTTED_Y_CHANNEL = 11;
 
 }
