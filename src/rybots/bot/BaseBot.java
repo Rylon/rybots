3184d944778d61d02345aa6e8fe8f5a50fe80b36
Ryan Conway <ryan@rjc.cc>
2017-03-28 01:08:10 +0100

    Copy the "willCollideWithMe" method from the example robot player.

--- src/rybots/bot/BaseBot.java
+++ src/rybots/bot/BaseBot.java
@@ -152,4 +152,37 @@ public abstract class BaseBot {
         return rc.canMove(direction) && !rc.hasMoved();
     }
 
+    /**
+     * A slightly more complicated example function, this returns true if the given bullet is on a collision
+     * course with the current robot. Doesn't take into account objects between the bullet and this robot.
+     *
+     * @param bullet The bullet in question
+     * @return True if the line of the bullet's path intersects with this robot's current position.
+     */
+    public boolean willCollideWithMe(BulletInfo bullet) {
+        MapLocation myLocation = rc.getLocation();
+
+        // Get relevant bullet information
+        Direction bulletDirection = bullet.dir;
+        MapLocation bulletLocation = bullet.location;
+
+        // Calculate bullet relations to this robot
+        Direction directionToRobot = bulletLocation.directionTo(myLocation);
+        float distToRobot = bulletLocation.distanceTo(myLocation);
+        float theta = bulletDirection.radiansBetween(directionToRobot);
+
+        // If theta > 90 degrees, then the bullet is traveling away from us and we can break early
+        if (Math.abs(theta) > Math.PI/2) {
+            return false;
+        }
+
+        // distToRobot is our hypotenuse, theta is our angle, and we want to know this length of the opposite leg.
+        // This is the distance of a line that goes from myLocation and intersects perpendicularly with propagationDirection.
+        // This corresponds to the smallest radius circle centered at our location that would intersect with the
+        // line that is the path of the bullet.
+        float perpendicularDist = (float)Math.abs(distToRobot * Math.sin(theta)); // soh cah toa :)
+
+        return (perpendicularDist <= rc.getType().bodyRadius);
+    }
+
 }
